4114d1db1404560c833af37812115b6e7b8d2c25
tsurubaso <tsurubaso@hotmail.com>
2025-03-20 00:32:10 +0900

    change on time line

--- app/timeline/page.jsx
+++ app/timeline/page.jsx
@@ -1,26 +1,10 @@
-// pages/index.js
-"use client";
-import React, { useState, useEffect } from 'react';
-import TimelineComponent from '../../components/Timeline';
-import NavBar from '../../components/NavBar';
-import supabase from "../../supabaseClient";
-
-
-
-
-
-
-//{ id: 4, content: 'Item 4', start: '2025-04-16', end: '2025-04-19', className: 'custom-class' , group: 1, type: 'range', style: 'background-color: blue; color: white;'}
-
-
 export default function HomePage() {
-
   const [items, setItems] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const { data, error } = await supabase
-      .from("stories") // Ensure you have a 'stories' table
+        .from("stories") // Ensure you have a 'stories' table
         .select('id, timelineStart, timelineEnd, description'); // Fetch relevant columns
 
       if (error) {
@@ -28,21 +12,21 @@ export default function HomePage() {
         return;
       }
 
-      const timelineItems = data.map((item) => ({
-        id: item.id,
-        content: item.description,
-        start: new Date(item.timelineStart).toISOString(),
-        end: item.timelineEnd ? new Date(item.timelineEnd).toISOString() : undefined,
-        // Add other properties if necessary
-      }));
+      const timelineItems = data
+        .filter(item => item.timelineStart) // Filter out items with null or undefined timelineStart
+        .map((item) => ({
+          id: item.id,
+          content: item.description,
+          start: new Date(item.timelineStart).toISOString(),
+          end: item.timelineEnd ? new Date(item.timelineEnd).toISOString() : undefined,
+          // Add other properties if necessary
+        }));
 
       setItems(timelineItems);
     };
 
     fetchData();
   }, []);
-
-
 
   return (
     <div className="min-h-screen bg-gray-50 p-4">
